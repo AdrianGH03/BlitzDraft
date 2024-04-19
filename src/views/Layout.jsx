8aacbeaf6824d0c8f86d18e84f94b1f1be6e023d
Miguel Gomez <miguelgomezca2021@gmail.com>
2024-04-19 19:09:21 -0400

    Pushing analytics

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import { Header } from '../components/Layout/Header';
 import { Footer } from '../components/Layout/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 export const Layout = ({ children }) => {
   return (
     <div className="layout-container">
       <Header />
         <div className="content">{children}</div>
+        <Analytics />
       <Footer />
     </div>
   );
