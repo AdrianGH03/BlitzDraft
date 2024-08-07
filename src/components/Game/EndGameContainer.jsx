122f20e69817c778f31cc3cba439c638f9519279
Miguel Gomez <miguelgomezca2021@gmail.com>
2024-08-06 21:46:22 -0400

    Pushing guess which team won feature

--- src/components/Game/EndGameContainer.jsx
+++ src/components/Game/EndGameContainer.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 
 //Contexts
@@ -28,6 +30,8 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
     const navigate = useNavigate();
     const { userInfo } = useContext(AuthContext);
     const [imagesLoaded, setImagesLoaded] = useState(false);
+    const [selectedTeam, setSelectedTeam] = useState('');
+    const [teamGuessed, setTeamGuessed] = useState(false);
 
 
     const lowscoreEmote = '/emotes/lowscore2.png';
@@ -59,12 +63,13 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
     }, []);
     
     useEffect(() => {
-        if(Object.keys(gameData).length > 0 && Object.keys(guesses).length > 0 && gameData.difficulty) {
+        if (Object.keys(gameData).length > 0 && Object.keys(guesses).length > 0 && gameData.difficulty) {
             const requestBody = {
                 gameData: gameData.gameData.body.game.data,
                 difficulty: gameData.difficulty,
                 guesses: guesses,
-                difficultySettings: gameData.gameData.body.difficultySettings
+                difficultySettings: gameData.gameData.body.difficultySettings,
+                selectedTeam: selectedTeam
             };
     
             if (userInfo) {
@@ -72,58 +77,84 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
             }
     
             fetchWithToken.post(`${import.meta.env.VITE_APP_CALCULATE_SCORE}`, requestBody)
-            .then(response => {
-                if(response === 400){
-                    navigate('/game/difficulty')
-                }
-                setScoreRange((response.data.totalScore / response.data.outOf) * 100)
-                setTotalPoints(response.data.outOf)
-                setActualScore(response.data.totalScore)
-                let scorePercentage = (response.data.totalScore / response.data.outOf) * 100;
-                let selectedEmotesArray;
-
-                if(scorePercentage < 33) {
-                    selectedEmotesArray = emotes.lowscore;
-                } else if(scorePercentage < 66) {
-                    selectedEmotesArray = emotes.medscore;
-                } else {
-                    selectedEmotesArray = emotes.highscore;
-                }
-
-                let randomEmote = selectedEmotesArray[0]; 
-                setEmote(randomEmote);
-
-                const imageUrls = [topIcon, jungleIcon, midIcon, botIcon, supportIcon, lowscoreEmote, highscoreEmote, medscoreEmote];
-                let imagesToLoad = imageUrls.length;
-
-                
-                if (gameData.gameData && gameData.gameData.body && gameData.gameData.body.teamImages) {
-                    imagesToLoad += Object.values(gameData.gameData.body.teamImages).length;
-                    Object.values(gameData.gameData.body.teamImages).forEach(url => {
-                        imageUrls.push(url);
-                    });
-                }
-
-                imageUrls.forEach(url => {
-                    const img = new Image();
-                    img.onload = () => {
-                        imagesToLoad--;
-                        if (imagesToLoad === 0) {
-                            setImagesLoaded(true);
+                .then(response => {
+                    if (response === 400) {
+                        navigate('/game/difficulty');
+                    }
+    
+                    let totalGuesses = response.data.outOf / 10;
+                    let correctGuesses = response.data.totalScore / 10;
+    
+                    if (selectedTeam) {
+                        totalGuesses += 1;
+                        if (gameData.gameData.body.game.data.Winner === selectedTeam) {
+                            correctGuesses += 1;
+                            correctGuesses -= 2;
                         }
-                    };
-                    img.src = url;
+                    }
+                    
+    
+                    
+                    setScoreRange((correctGuesses / totalGuesses) * 100);
+                    setTotalPoints(response.data.outOf);
+                    setActualScore(response.data.totalScore);
+    
+                    let scorePercentage = (correctGuesses / totalGuesses) * 100;
+                    let selectedEmotesArray;
+    
+                    if (scorePercentage < 33) {
+                        selectedEmotesArray = emotes.lowscore;
+                    } else if (scorePercentage < 66) {
+                        selectedEmotesArray = emotes.medscore;
+                    } else {
+                        selectedEmotesArray = emotes.highscore;
+                    }
+    
+                    let randomEmote = selectedEmotesArray[0];
+                    setEmote(randomEmote);
+    
+                    const imageUrls = [topIcon, jungleIcon, midIcon, botIcon, supportIcon, lowscoreEmote, highscoreEmote, medscoreEmote];
+                    let imagesToLoad = imageUrls.length;
+    
+                    if (gameData.gameData && gameData.gameData.body && gameData.gameData.body.teamImages) {
+                        imagesToLoad += Object.values(gameData.gameData.body.teamImages).length;
+                        Object.values(gameData.gameData.body.teamImages).forEach(url => {
+                            imageUrls.push(url);
+                        });
+                    }
+    
+                    imageUrls.forEach(url => {
+                        const img = new Image();
+                        img.onload = () => {
+                            imagesToLoad--;
+                            if (imagesToLoad === 0) {
+                                setImagesLoaded(true);
+                            }
+                        };
+                        img.src = url;
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error);
                 });
-            })
-            .catch(error => {
-            console.error('Error:', error);
-            });
         }
-    }, []);
+    }, [selectedTeam]);
+
+    const handleTeamSelection = (team) => {
+        setSelectedTeam(team);
+    };
+    const handleDisableGuess = () => {
+        setTeamGuessed(true);
+    }
+    const handleWinnerguess = (team) => {
+        handleTeamSelection(team);
+        handleDisableGuess();
+    }
 
     const goToGame = () => {
         navigate('/game/difficulty')
     }
+   
     
   return (
     <>
@@ -148,8 +179,51 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
                         <p className="end-game-details-text">TOTAL: {Number.isInteger(actualScore) ? actualScore : Number(actualScore).toFixed(2)}/{Number.isInteger(totalPoints) ? totalPoints : Number(totalPoints).toFixed(2)}</p>
                         <p className="end-game-details-text">REGION: {gameData.gameData.body.region}</p>
                         <p className="end-game-details-text">PATCH: {gameData.gameData.body.game.data.Patch}</p>
-                        <button className="end-game-details-playagain-button" onClick={() => goToGame()}>
-                            PLAY AGAIN
+                        
+                        <section className="end-game-guess">
+                            <h2>Guess Winner +20 PTS</h2>
+                            <div className="">
+                                <button className="end-game-details-playagain-button endgameblue" onClick={() => handleWinnerguess("1")}
+                                    style={{
+                                        backgroundColor: 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "1" ? 'lime' : 
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "1" ? 'red' : 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "2" ? 'lime' :
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "2" ? 'red' :
+                                        'blue'
+                                    }}
+                                    disabled={selectedTeam}
+                                >
+                                    {
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "1" ? <FontAwesomeIcon icon={faCheck} /> : 
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "1" ? <FontAwesomeIcon icon={faXmark} /> :
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "2" ? <FontAwesomeIcon icon={faXmark} /> : 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "2" ? <FontAwesomeIcon icon={faCheck} /> : null
+                                    }
+                                    BLUE
+                                </button>
+                                <button className="end-game-details-playagain-button endgamered" onClick={() => handleWinnerguess("2")}
+                                    style={{
+                                        backgroundColor: 
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "2" ? 'lime' : 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "2" ? 'red' : 
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "1" ? 'lime' :
+                                        'red'
+                                    }}
+                                    disabled={selectedTeam}
+                                >
+                                    {
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "2" ? <FontAwesomeIcon icon={faCheck} /> : 
+                                        gameData.gameData.body.game.data.Winner === "2" && selectedTeam === "1" ? <FontAwesomeIcon icon={faCheck} /> : 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "2" ? <FontAwesomeIcon icon={faXmark} /> : 
+                                        gameData.gameData.body.game.data.Winner === "1" && selectedTeam === "1" ? <FontAwesomeIcon icon={faXmark} /> : null
+                                    }
+                                    RED
+                                </button>
+                            </div>
+                        </section>
+                       <button className="end-game-details-playagain-button" onClick={() => goToGame()}>
+                            NEW GAME
 
                         </button>
                     </div>
@@ -167,7 +241,7 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
                                 >{team.toUpperCase()}</span>
                                 {gameData.gameData.body.playerImages[team].map((player, playerIndex) => (
                                     <div key={playerIndex}>
-                                        <img src={player.image ? player.image : placeholder} alt={player.player} className='player-image' />
+                                        <img src={player == null ? placeholder : player.image ? player.image : placeholder} alt={player == null ? 'placeholder' : player.player} className='player-image' />
                                         <img src={icons[playerIndex]} alt='role icon' className='role-icon' />
                                     </div>
                                 ))}
@@ -176,6 +250,7 @@ export const EndGameContainer = ({guesses, gameData, fetchWithToken, showEndGame
                     }
                 </section>
 
+                
 
 
             </section>
